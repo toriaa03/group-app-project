8115e5811a60411df3b9bd9bd35cac1856d1700e
Koy Herrick <herrickoy@gmail.com>
2020-04-13 10:56:55 -0600

    git ignore and package-lockjson and profile page add

---
+++ src/components/pages/profile.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+
+
+
+
+
+export default class ProfilePage extends Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          
+      }
+  }
+
+//   create posts
+// profile picture
+// their past posts
+// friends section
+// pictures section
+// about
+// bio/details
+
+  render() {
+      return (
+          <div className="profilePageWrapper">
+            <div className="profileHeading">
+                <div className="profileBackground">
+                    
+                </div>
+                <div className="profilePicture">
+                    
+
+                    </div>
+                <button className="edit">EDIT</button>
+                <div class="profileOptions">
+                    <div className="about">About</div>
+                    <div className="friends">Friends</div>
+                    <div className="photos">Photos</div>
+                </div>
+
+            </div>
+
+            create
+
+            <div className="posts">
+
+            </div>
+
+              
+          </div>
+      )
+  }
+}
